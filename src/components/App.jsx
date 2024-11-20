bc493a339f11b2717ce1d74b7abd733f417ed1ac
AloBordo1992 <168308266+YuraShybalov@users.noreply.github.com>
2024-11-20 21:06:18 +0100

    push

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,13 +2,15 @@ import Wrapper from './Wrapper/wrapper';
 import Profile from './Profile/pofile';
 import Statistics from './Statistics/statistics';
 import FriendList from './FriendList/friendList';
+import TransactionHistory from './TransactionHistory/transactionHistory';
 
 import data from 'files/data.json';
 import friends from 'files/friends.json';
+import transactions from 'files/transactions.json';
 
 export const App = () => {
   return (
-    <div>
+    <>
       <Wrapper title={"Task-1 'Social network profile'"}>
         <Profile />
       </Wrapper>
@@ -18,6 +20,9 @@ export const App = () => {
       <Wrapper title={"Task-3 'Friend list'"}>
         <FriendList friends={friends} />
       </Wrapper>
-    </div>
+      <Wrapper title={"Task-4 'Transaction history'"}>
+        <TransactionHistory transactions={transactions} />
+      </Wrapper>
+    </>
   );
 };
